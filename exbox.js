9ff460a0ae4307fefce56268f1789259902f08c6
luke <luke.edwards05@gmail.com>
2016-04-28 12:16:20 -0700

    begin cli script, stubs for init & domain

---
+++ exbox.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict';
+
+var cli = require('commander');
+var readPkg = require('read-pkg');
+var notifier = require('update-notifier');
+
+var pkg = readPkg.sync(__dirname);
+notifier({pkg: pkg}).notify();
+
+cli.version(pkg.version)
+	.usage('<command> [args...] [options]')
+	.on('--help', function () {
+		console.log('  For additional help with any command, run `exbox COMMAND -h`');
+		console.log();
+	});
+
+cli
+	.command('init')
+	.description('setup ExBox for the first time')
+	.action(function () {
+		console.log('inside init!');
+	});
+
+cli
+	.command('domain <site> <dir>')
+	.description('Map a Domain to a VM directory')
+	.option('--ssl', '     Enable SSL on this domain')
+	.action(function (site, dir, opts) {
+		console.log('use ssl?', opts.ssl || false);
+		console.log('this is site: ', site);
+		console.log('this is dir: ', dir);
+	}).on('--help', function () {
+		console.log('  Examples:');
+		console.log();
+		console.log('    $ exbox domain phoenix.dev /home/vagrant/code/phoenix');
+		console.log('    $ exbox domain hello-world.app /home/vagrant/code/hello-world');
+		console.log('    $ exbox domain --ssl secure.app /home/vagrant/code/secure');
+		console.log('    $ exbox domain secure.app /home/vagrant/code/secure --ssl');
+		console.log();
+	});
+
+cli.parse(process.argv);
