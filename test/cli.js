b6093bec76ecbb72d7bd5694ef475f9a24ffdca2
luke <luke.edwards05@gmail.com>
2016-04-28 12:16:41 -0700

    begin cli tests

--- test/cli.js
+++ test/cli.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
-// import path from 'path';
+import {resolve} from 'path';
 import write from 'temp-write';
-// import execa from 'execa';
+import readPkg from 'read-pkg';
+import execa from 'execa';
 import test from 'ava';
 
 global.Promise = Promise;
+const cli = resolve('../exbox.js');
 
-test('fix option', async t => {
+test('dummy test', async t => {
 	const filepath = await write('console.log(0)\n', 'x.js');
 	t.is(fs.readFileSync(filepath, 'utf8').trim(), 'console.log(0)');
+});
+
+test('display current CLI version', async t => {
+	const out = await execa.stdout(cli, ['--version']);
+	t.is(out, readPkg.sync('..').version);
 });
